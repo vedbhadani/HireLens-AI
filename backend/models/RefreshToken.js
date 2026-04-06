a3946ba40a025c5d127a26049be8e80cdbdf78df
vedbhadani <vedbhadani157@gmail.com>
2026-04-07 02:46:04 +0530

    feat: RefreshToken model with TTL index

---
+++ backend/models/RefreshToken.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const refreshTokenSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+    index: { expires: 0 },
+  },
+  isRevoked: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model('RefreshToken', refreshTokenSchema);
