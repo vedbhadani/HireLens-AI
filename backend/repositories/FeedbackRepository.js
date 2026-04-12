09a8f4ead406dd9c2bd0ec1ac354eb2d8be4b42c
vedbhadani <vedbhadani157@gmail.com>
2026-04-12 13:35:01 +0530

    feat: FeedbackRepository

---
+++ backend/repositories/FeedbackRepository.js
@@ -0,0 +1,25 @@
+const Feedback = require('../models/Feedback');
+
+class FeedbackRepository {
+  async create(data) {
+    const feedback = new Feedback(data);
+    return await feedback.save();
+  }
+
+  async findByCandidate(candidateId) {
+    return await Feedback.find({ candidateId })
+      .populate('recruiterId', 'name email')
+      .populate('jobId', 'title company')
+      .sort({ createdAt: -1 });
+  }
+
+  async findByJob(jobId) {
+    return await Feedback.find({ jobId }).sort({ createdAt: -1 });
+  }
+
+  async findExisting(recruiterId, candidateId, jobId) {
+    return await Feedback.findOne({ recruiterId, candidateId, jobId });
+  }
+}
+
+module.exports = new FeedbackRepository();
